3392525fd124a1d1309f61b077b4f09d3cfe66a4
Ben Saward <benjamin.saward@gmail.com>
2013-03-05 23:26:42 +0000

    modified:   rocks.c

--- rocks.c
+++ rocks.c
@@ -6,16 +6,36 @@
 #define random_max 9
 #define random_min 0
 #define bin_number (random_max-random_min)+1
-#define bin[bin_number] 0
 
+int bin_array[bin_number];
+
+int buffer_init();
+int buffer_update();
+int buffer_print();
+int screen_init();
+int buffer_init()
+{
+  for (int i=0; i<bin_number; i++)
+  {
+    bin_array=0;
+  }
+}
+
+int screen_init()
+{
+  int n=random_min, triple_i;
+  for (int i=random_min; i=<random_max; i++)
+  {
+    triple_i=i*3;
+    printf("\033[2;%dH%d", triple_i, i);
+    printf("\033[1;%dH%d", triple_i, n);
+    n++;
+  }
+  
 int main ()
 {
-	for (int i=random_min; i=<random_max; i++)
-	{
-		printf("\033[2;%dH%d", i, i);
-	}
-   
-   for (;;)
-   {
-      int random = rand_int()
+  for (;;)
+  {
+    int random = rand_int()
+  }
 }
