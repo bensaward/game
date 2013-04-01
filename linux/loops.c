bb7215b59d20c8622e6b5da825daafc85ad21910
Ben Saward <benjamin.saward@gmail.com>
2013-04-01 04:13:27 +0100

    Added print_n_xml(), wait_for_enter(), separated print_screen() into 2 parts in order to have a nice ascii drawing intro to the game.

--- linux/loops.c
+++ linux/loops.c
@@ -1,7 +1,11 @@
 #include <main.h>
+#include <string.h>
+#include <launcher_src/launcher.h>
 
 void loop_campaign()
 {
+  char *campaign_type="regular";
+  intro_loop(campaign_type);
   init_coords();
   draw_screen();
   get_config();
@@ -23,3 +27,23 @@ void loop_menu()
     update_cursor(character_pressed);
   }
 }
+
+void intro_loop(char *campaign)
+{
+  if (strncmp(campaign, "regular", sizeof(campaign))==0)
+  {
+    int number_xml=3;
+    getcwd(cwd, sizeof(cwd));
+    strncat(cwd, "/../xml", sizeof(cwd));
+    char file_names[3][16]={"0.xml", "1.xml", "2.xml"};
+    char file_address[3][128];
+    for (int i=0; i<number_xml; i++)
+    {
+      strncpy(file_address[i], cwd, sizeof(file_address[i]));
+      strncat(file_address[i], file_names[i], sizeof(file_address[i]));
+     // printf("\033[%d;1Hfile_address[%d]=\"%s\"", i+1, i, file_address[i]);
+    }
+  }
+}
+    
+    
--- linux/print.c
+++ linux/print.c
@@ -3,56 +3,6 @@
 #include <stdlib.h>
 #include <main.h>
 
-int getch(void)
-{
-  struct termios oldattr, newattr;
-  int ch;
-  tcgetattr(STDIN_FILENO, &oldattr);
-  newattr=oldattr;
-  newattr.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
-  ch=getchar();
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
-  return ch;
-}
-
-
-void update_coords (int key)
-{
-  old_x=coordinate_x;
-  old_y=coordinate_y;
-  if (key==up_key)
-  {
-    if(coordinate_y!=1)
-    {
-      coordinate_y--;
-    }
-  }
-  
-  if(key==left_key)
-  {   
-    if (coordinate_x!=1)
-    {
-      coordinate_x--;  
-    }
-  }
-  
-  if(key==down_key)
-  {
-    if (coordinate_y!=array_height)
-    {
-      coordinate_y++;
-    }
-  }
-
-  if(key==right_key)
-  {
-    if (coordinate_x!=array_width)
-      {
-	coordinate_x++;
-      }    
-  }
-}
 
 void print(int x, int y)
 {
@@ -103,12 +53,7 @@ void draw_screen ()
     }
   }
   printf("\n");
-  for (int x=0; x<array_width; x++)
-  {
-    printf("\033[%d;%dH-", array_height+2, x);
-    printf("\033[%d;%dH-", array_height+4, x);   
-  }
-  printf("\033[%d;1H>> ", array_height+3);  
+  print_terminal();
 }
 
 void print_menu()
@@ -124,66 +69,32 @@ void print_menu()
   printf("\033[%d;%dH%c", cursor_y, array_width-1, cursor_char);
 }
 
-void update_cursor(int pressed)
+
+void print_n_xml(char *file_path, char *buffer, int buffer_size)
 {
-  int old_cursor_y=cursor_y;
-  //printf("\033[10;1HKey_pressed=%d", pressed);                   //use for debuging getch() and similar functions
-  //printf("\033[11;1HExtended_key=%d", extended_key(pressed));    //use for debuging getch() and similar functions
-  switch (extended_key(pressed))
+  FILE *xml;
+  int line_count=1;
+  xml=fopen(file_path, "rt");
+  if(xml != NULL)
   {
-    case arrow_up:
+    while(fgets(buffer, buffer_size, xml)!=NULL)
     {
-      if (cursor_y!=2)
-      {
-	cursor_y-=2;
-	break;
-      }
-      else
-      {
-	break;
-      }
-    }
-    
-    case arrow_down:
-    {
-      if (cursor_y!=6)
-      {
-	cursor_y+=2;
-	break;
-      }
-      else
-      {
-	break;
-      }
-    }
-   
-    case enter_key:
-    {
-      switch(cursor_y)
-      {
-	case 2:
-	  loop_campaign();
-	  break;
-	  
-	case 4:
-	  system("clear");
-	  printf("\033[1;1H");
-	  printf("See ~/.game/config.txt for options!\nPress any key to return...");
-	  getch();
-	  loop_menu();
-	  break;
-	  
-	case 6:
-	  printf("\n\n");
-	  exit(0);
-      }
+     printf("\033[%d;1H%s", line_count, buffer);
+     line_count++;
     }
   }
-  printf("\033[%d;%dH%c", old_cursor_y, array_width-1, ' ');
-  printf("\033[%d;%dH%c", cursor_y, array_width-1, cursor_char);
+  else
+  {
+    printf("\033[%d;1HError in opening xml %s", array_height+10, file_path);
+  }
 }
 
-int extended_key(int pressed)
+void print_terminal(void)
 {
-  return (pressed==0 || pressed==244) ? getch() : pressed;
+  for (int x=0; x<array_width; x++)
+  {
+    printf("\033[%d;%dH-", array_height+2, x);
+    printf("\033[%d;%dH-", array_height+4, x);   
+  }
+  printf("\033[%d;1H>> ", array_height+3);  
 }
